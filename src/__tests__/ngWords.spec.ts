58338fa6dae57a540a60fc528abc895090326444
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2021-05-25 19:48:30 +0200

    WIP

--- src/__tests__/ngWords.spec.ts
+++ src/__tests__/ngWords.spec.ts
@@ -1,3 +1,4 @@
+import { unifyCyrillicNG } from '../exceptions/ngWords';
 import { unifyBigrams } from '../helpers/latinBigramHelpers';
 import {
   N_TILDE_SMALL as n_,
@@ -44,6 +45,51 @@ describe('`NG`-ended nouns with suffixes and exceptions', () => {
     ${"go'ngingning"}   | ${`g${o_}${n_}i${n_}ni${n_}`}
   `('[$input] should be transformed into [$output]', ({ input, output }) => {
     const result = unifyBigrams(input);
+
+    expect(result).toBe(output);
+  });
+});
+
+describe('cyrillic `NG`-word splitting and exceptions', () => {
+  test.each`
+    input                | output
+    ${'кўнгил'}          | ${`кў${n_}ил`}
+    ${'кўнгилсиз'}       | ${`кў${n_}илсиз`}
+    ${'бекўнгилдан'}     | ${`бекў${n_}илдан`}
+    ${'бодрингингнинг'}  | ${`бодри${n_}и${n_}ни${n_}`}
+    ${'пулингиз'}        | ${`пули${n_}из`}
+    ${'пулингизга'}      | ${`пули${n_}изга`}
+    ${'тунги'}           | ${`тунги`}
+    ${'тонги'}           | ${`то${n_}и`}
+    ${'денгиздан'}       | ${`де${n_}издан`}
+    ${'олингизи'}        | ${`оли${n_}изи`}
+    ${'олингизидан'}     | ${`оли${n_}изидан`}
+    ${'олингизларининг'} | ${`оли${n_}изларини${n_}`}
+    ${'сингилингизнинг'} | ${`си${n_}или${n_}изни${n_}` /* It has grammar mistake, just for testing purposes.*/}
+  `('[$input] should be transformed into [$output]', ({ input, output }) => {
+    const result = unifyCyrillicNG(input);
+
+    expect(result).toBe(output);
+  });
+});
+
+describe('cyrillic `NG`-ended nouns with suffixes and exceptions', () => {
+  test.each`
+    input               | output
+    ${'инглизнинг'}     | ${`инглизни${n_}`}
+    ${'бодрингингнинг'} | ${`бодри${n_}и${n_}ни${n_}`}
+    ${'рангингнинг'}    | ${`ра${n_}и${n_}ни${n_}`}
+    ${'тенгингнинг'}    | ${`те${n_}и${n_}ни${n_}`}
+    ${'гарангингнинг'}  | ${`гара${n_}и${n_}ни${n_}`}
+    ${'гурунгингнинг'}  | ${`гуру${n_}и${n_}ни${n_}`}
+    ${'тонгингнинг'}    | ${`то${n_}и${n_}ни${n_}`}
+    ${'мингингнинг'}    | ${`ми${n_}и${n_}ни${n_}`}
+    ${'рингингнинг'}    | ${`ри${n_}и${n_}ни${n_}`}
+    ${'енгингнинг'}     | ${`е${n_}и${n_}ни${n_}`}
+    ${'енгизлар'}       | ${`е${n_}излар`}
+    ${'гўнгингнинг'}    | ${`гў${n_}и${n_}ни${n_}`}
+  `('[$input] should be transformed into [$output]', ({ input, output }) => {
+    const result = unifyCyrillicNG(input);
 
     expect(result).toBe(output);
   });
--- src/exceptions/ngWords.ts
+++ src/exceptions/ngWords.ts
@@ -6,33 +6,57 @@ import {
 // [EN] Words where 'NG' comes as a single consonant. This list is not complete. Feel free to send PR for improvements.
 // [UZ] 'NG' tovush sifatida kelgan so'zlar. Ro'yhat to'liq emas. Agar tuzatish kiritishni xohlasangiz bemalol PR jonatishingiz mumkin.
 const NG_WORDS = [
-  `k${o_}ngil`,
-  'singil',
-  'dengiz',
-  'bodring',
-  'rang',
-  'teng',
-  'garang',
-  'gurung',
-  'tong',
-  'ming',
-  'ring',
-  'yeng', // noun
-  `g${o_}ng`,
+  [`k${o_}ngil`, 'кўнгил'],
+  ['singil', 'сингил'],
+  ['dengiz', 'денгиз'],
+  ['bodring', 'бодринг'],
+  ['rang', 'ранг'],
+  ['teng', 'тенг'],
+  ['garang', 'гаранг'],
+  ['gurung', 'гурунг'],
+  ['tong', 'тонг'],
+  ['ming', 'минг'],
+  ['ring', 'ринг'],
+  ['yeng', 'енг'], // noun
+  [`g${o_}ng`, 'гўнг'],
 ];
 
-const SUFFIXES = ['ning', 'ni', 'ga', 'da', 'dan'];
-const SUFFIXES_JOIN = SUFFIXES.join('|');
+const SUFFIXES = [
+  ['ning', 'нинг'],
+  ['ni', 'ни'],
+  ['ga', 'га'],
+  ['da', 'да'],
+  ['dan', 'дан'],
+];
 
-const NG_WORDS_JOIN = NG_WORDS.join('|');
-const NG_REGX = new RegExp(
-  `${NG_WORDS_JOIN}|[ia]ng(iz)?(${SUFFIXES_JOIN})?(lar?)?i?(${SUFFIXES_JOIN})?$`,
-  'g',
+const LATIN_SUFFIXES_JOIN = SUFFIXES.map(([latin]) => latin).join('|');
+const LATIN_NG_WORDS_JOIN = NG_WORDS.map(([latin]) => latin).join('|');
+const LATIN_NG_REGX = new RegExp(
+  `${LATIN_NG_WORDS_JOIN}|[ia]ng(iz)?(${LATIN_SUFFIXES_JOIN})?(lar?)?i?(${LATIN_SUFFIXES_JOIN})?$`,
+  'gi',
 );
 
 /** Replace `ng` bigram with a special character */
-export function unifyNG(word: string): string {
-  return word.replace(NG_REGX, (matchString) =>
-    matchString.replace(/ng/g, N_TILDE_SMALL),
+export function unifyLatinNG(word: string): string {
+  return word.replace(LATIN_NG_REGX, (matchString) =>
+    matchString.replace(/ng/gi, N_TILDE_SMALL),
   );
+}
+
+const CYRILLIC_SUFFIXES_JOIN = SUFFIXES.map(([, cyr]) => cyr).join('|');
+const CYRILLIC_NG_WORDS_JOIN = NG_WORDS.map(([, cyr]) => cyr).join('|');
+const CYRILLIC_NG_REGX = new RegExp(
+  `${CYRILLIC_NG_WORDS_JOIN}|[иа]нг(из)?(${CYRILLIC_SUFFIXES_JOIN})?(лар?)?и?(${CYRILLIC_SUFFIXES_JOIN})?$`,
+  'gi',
+);
+
+/** Replace `нг` bigram with a special character */
+export function unifyCyrillicNG(word: string): string {
+  return word.replace(CYRILLIC_NG_REGX, (matchString) =>
+    matchString.replace(/нг/gi, N_TILDE_SMALL),
+  );
+}
+
+export function splitCyrillicNG(word: string): string {
+  return word.replace(new RegExp(N_TILDE_SMALL, 'g'), 'нг');
 }
